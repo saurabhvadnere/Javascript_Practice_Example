0f54ac07e359a78b1ec2506992739a5454c4e3dd
saurabhvadnere <102731958+saurabhvadnere@users.noreply.github.com>
2024-03-01 22:27:35 +0530

    FInd Square root

---
+++ Square Root.js
@@ -0,0 +1,14 @@
+// take the input from the user
+// const number = prompt('Enter the number: '); // Prompt is not working in VS Code
+
+const number = 25;
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
+//Here, the Math.sqrt() method takes a number and returns its square root.
+
+
+/*If 0 or a positive number is passed in the Math.sqrt() method, 
+  then the square root of that number is returned.  */
+// If a negative number is passed, NaN is returned.
+// If a string is passed, NaN is returned.
