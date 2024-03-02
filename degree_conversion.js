947697bc18c66ab7f9c8769ea8a5757b1bfc122f
saurabhvadnere <102731958+saurabhvadnere@users.noreply.github.com>
2024-03-02 22:44:24 +0530

    Fahrenheit to Celsius

---
+++ degree_conversion.js
@@ -0,0 +1,18 @@
+let Fahrenheit = 80;
+
+const factor = ((Fahrenheit - 32 ) / 1.8).toFixed(1);
+
+let celsius = factor;
+
+console.log(`${Fahrenheit} Fahrenheit degree is equal to ${celsius} celsius degree. `)
+
+
+// program to convert celsius to Fah
+// ask the celsius value to the user 
+const Cel = 24;
+
+// calculate Fah
+const Fah = (Cel * 1.8) + 32
+
+// display the result
+console.log(`${Cel} degree celsius is equal to ${Fah} degree fahrenheit.`);
