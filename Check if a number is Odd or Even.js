bd3823a5aaafb60faa03879ccc9fbc12e850ca59
saurabhvadnere <102731958+saurabhvadnere@users.noreply.github.com>
2024-03-08 22:05:51 +0530

    Check if a number is Odd or Even

---
+++ Check if a number is Odd or Even.js
@@ -0,0 +1,16 @@
+// program to check if the number is even or odd
+// take input from the user
+// const number = prompt("Enter a number: ");
+const number = 6576879787898
+
+//check if the number is even
+// Example 1: Using if...else
+
+if(number % 2 == 0) {
+    console.log("The number is even.");
+}
+
+// if the number is odd
+else {
+    console.log("The number is odd.");
+}
