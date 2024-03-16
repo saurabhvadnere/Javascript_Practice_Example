a6de1d192306faee7af39f7c1c55696de6f0393f
saurabhvadnere <102731958+saurabhvadnere@users.noreply.github.com>
2024-03-16 22:25:08 +0530

    Sum of Number

---
+++ Sum of Number.js
@@ -0,0 +1,21 @@
+// Exmaple 1 using while loop
+let total = 0;
+let i = 1;
+let number=10;//+prompt("Enter the number")
+
+while (i<=number) {
+    total +=i;
+    i++;
+}
+console.log("the sum numbers is",total)
+
+
+// // Exmaple 2 using for loop
+
+let totals = 0;
+let numbers=100;//+prompt("Enter the number")
+
+for (let i=1; i<=numbers; i++){
+    totals += i;
+}
+console.log("the sum numbers is",totals)
